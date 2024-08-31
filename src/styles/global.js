1791bf612fe6f233e884b81a79b7cc04118a3936
Andre Parron <andreparoon@gmail.com>
2024-08-30 22:57:27 -0300

    feat: Criação e configuração estilo global, thema e DefaultLayout

---
+++ src/styles/global.js
@@ -0,0 +1,97 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+}
+body, html {
+    height: 100%;
+    background-color: ${props => props.theme["gray-100"]};
+  }
+
+  body {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+button{
+    cursor: pointer;
+}
+
+a{
+    color: inherit;
+    text-decoration: none;
+}
+
+
+@media (max-width: 1080px){
+    html{
+        font-size: 93.75%;
+    }
+}
+@media (max-width: 720px){
+    html{
+        font-size: 87.5%;
+    }
+}
+
+
+.react-modal-overlay {
+    background-color: rgba(0, 0, 0, 0.7); 
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.react-modal-content {
+    width: 100%;
+    max-width: 36rem;
+    background-color: ${props => props.theme["gray-700"]}; 
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.5rem; 
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25); 
+}
+
+.react-modal-close {
+    position: absolute;
+    right: 1.2rem;
+    top: 1.2rem;
+    border: 0;
+    background: transparent;
+
+    color: ${props => props.theme["gray-500"]}; 
+
+    transition: filter 0.2s;
+    &:hover {
+        filter: brightness(0.8); 
+    }
+}
+
+.Toastify__toast--error {
+    background: ${props => props.theme["red-700"]};
+    color: ${props=> props.theme["gray-100"]};
+}
+
+.Toastify__toast--success {
+    background: ${props => props.theme["green-500"]};
+    color: ${props=> props.theme["gray-100"]};
+  }
+
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,31 @@
+export const defaultTheme = {
+    'white': '#fff',
+
+    'gray-100': '#E1E1E6',
+    'gray-150': '#D9D9DC',
+    'gray-200': '#D1D1D6',
+    'gray-250': '#CBCBCF',
+    'gray-300': '#C4C4CC',
+    'gray-400': '#8D8D99',
+    'gray-500': '#7C7C8A',
+    'gray-600': '#323238',
+    'gray-700': '#29292E',
+    'gray-800': '#202024',
+    'gray-900': '#121214',
+
+    'green-300': '#00B37E',
+    'green-500': '#00875F',
+    'green-700': '#015F43',
+
+    'blue-700': '#0056b3',
+    'blue-600': '#0069d9',
+    'blue-500': '#007bff',
+
+
+    'red-300': '#E34A59',
+    'red-500': '#AB222E',
+    'red-700': '#7A1921',
+
+    'yellow-500': '#FBA94C'
+
+}
