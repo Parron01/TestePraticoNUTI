140e959946c4643f84f1dc9c6fb4173de205c555
Andre Parron <andreparoon@gmail.com>
2024-09-01 09:57:29 -0300

    feat: Inicialização projeto Back End.

---
+++ BACKEND/src/main/java/com/projetoNuti/ProjetoBackEndNutiApplication.java
@@ -0,0 +1,13 @@
+package com.projetoNuti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackEndNutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackEndNutiApplication.class, args);
+	}
+
+}
---
+++ BACKEND/src/test/java/com/projetoNuti/ProjetoBackEndNutiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoNuti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackEndNutiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
