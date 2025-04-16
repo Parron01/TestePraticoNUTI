9e982459140498b89f80b870a281666f995a057a
Andre Parron <andreparoon@gmail.com>
2025-04-16 18:24:48 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // baseURL: 'http://localhost:8080',
-    baseURL: 'https://nuti.parron01.com/api',
+    baseURL: 'http://localhost:8080',
+    // baseURL: 'https://nuti.parron01.com/api',
 });
