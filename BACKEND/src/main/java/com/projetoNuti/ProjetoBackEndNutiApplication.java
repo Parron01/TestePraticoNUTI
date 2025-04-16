0bd23425c286eb2678840b1e988ca006247d1a65
Andre Parron <andreparoon@gmail.com>
2025-04-16 18:16:42 -0400

    config: configurando cors

--- BACKEND/src/main/java/com/projetoNuti/ProjetoBackEndNutiApplication.java
+++ BACKEND/src/main/java/com/projetoNuti/ProjetoBackEndNutiApplication.java
@@ -2,6 +2,9 @@ package com.projetoNuti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProjetoBackEndNutiApplication {
@@ -10,4 +13,17 @@ public class ProjetoBackEndNutiApplication {
 		SpringApplication.run(ProjetoBackEndNutiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000", "https://nuti.parron01.com")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
